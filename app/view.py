a8f71eb09472cb4c081dfbc4fc4895cea22d35e7
williamhuang <chobits5468@gmail.com>
2017-02-14 16:38:23 +0800

    puppies init

--- app/view.py
+++ app/view.py
@@ -1,5 +1,21 @@
 from app import app
+from flask import jsonify, abort
 
-@app.route("/")
-def index():
-    return "hello"
+PUPPIES = [
+    {
+        "name" : "Rover",
+        "image_url" : "http://example.com/rover.jpg",
+    },
+    {
+        "name" : "Spot",
+        "image_url" : "http://example.com/spot.jpg",
+    },
+]
+
+@app.route("/<int:index>")
+def get_puppy(index):
+    try:
+        puppy = PUPPIES[index]
+    except IndexError:
+        abort(404)
+    return jsonify(puppy)
