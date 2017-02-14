e75130899de7df2ef23133ab0ac5d8f934cf4d25
williamhuang <chobits5468@gmail.com>
2017-02-14 19:02:57 +0800

    add sql part, include model, and sql command, but the filer finish not yet

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PuppyPicture
 follow Pycon 2016 speak - [Prototyping New APIs with Flask](https://www.youtube.com/watch?v=6RdZNiyISVU)
+
+
+## Day one 
+
+simple api, return json
+
+## Day two 
+
+Include FlaskSQLAlchemy
---
+++ app/sqlmodel.py
@@ -0,0 +1,62 @@
+import psycopg2
+
+def create_table():
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+
+        # user id PRMIARY KEY, username unike
+        cursor.execute("CREATE TABLE puppytable (id SERIAL PRIMARY KEY, slug char(32), name char(32), image_url char(128));")
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print("Uh oh, can't connect. Invalid dbname, user or password?")
+        print(e)
+        cursor.close()
+        conn.close()
+
+def create_puppy(slug, name, image_url):
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+
+        cursor.execute("INSERT INTO puppytable (slug, name, image_url) VALUES (%s, %s, %s)", (slug, name, image_url))
+
+        cursor.execute("SELECT * from puppytable")
+        row = cursor.fetchall()
+        print(row)
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print("Uh oh, can't connect. Invalid dbname, user or password?")
+        print(e)
+        cursor.close()
+        conn.close()
+
+def read(slug):
+    try:
+        conn = connect()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * from puppytable WHERE puppytable.name = "+"Rover")
+        row = cursor.fetchall()
+        print(row)
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print("Uh oh, can't connect. Invalid dbname, user or password?")
+        print(e)
+        cursor.close()
+        conn.close()
+
+def connect():
+    connect_str = "dbname=puppydb user=william host=localhost " + \
+                  "password=wflu"
+    conn = psycopg2.connect(connect_str)
+    return conn
+
+read("spot")
